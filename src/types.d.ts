6290014689f57f2f7b6f1c475162a5afc61824e0
th0r <grunin.ya@ya.ru>
2019-04-29 13:55:53 -0700

    Add descriptions for options

--- src/types.d.ts
+++ src/types.d.ts
@@ -6,6 +6,11 @@ export type IconMatcher = [
 ];
 
 export interface AngularSvgIconsOptions {
-  iconFilePathById: (iconId: string) => string;
+  // Function that must return an *absolute* path to the icon by its ID.
+  // It may also return `null` or `undefined` to ignore this icon and and don't add and `import` statement for it.
+  iconFilePathById: (iconId: string) => string | undefined | null;
+  // List of icon matchers. Each of them should be a string in the following format: <component-name attr-name>.
+  // E.g. "<app-svg-icon iconId>" matcher will search for `app-svg-icon` components and get icon ID from its
+  // `iconId` attributes.
   iconMatchers: string[];
 }
