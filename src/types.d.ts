302e7054bb8ce510d4cea736450edad7e4471039
th0r <grunin.ya@ya.ru>
2019-04-30 13:12:15 -0700

    Add notes on why we use serializable options (to support `thread-loader`)

--- src/types.d.ts
+++ src/types.d.ts
@@ -8,6 +8,7 @@ export type IconMatcher = [
 export interface AngularSvgIconsOptions {
   // Template string that converts icon ID into an *absolute* path to the icon.
   // `[id]` placeholder will be replaced by the icon ID.
+  // Using string template here to support `thread-loader`
   iconFilePath: string;
   // List of icon matchers. Each of them should be a string in the following format: <component-name attr-name>.
   // E.g. "<app-svg-icon iconId>" matcher will search for `app-svg-icon` components and get icon ID from its
@@ -15,5 +16,6 @@ export interface AngularSvgIconsOptions {
   iconMatchers: string[];
   // Strings, containing regexps matching icon IDs that should will be ignored.
   // E.g. '^(data|code)-'
+  // Using strings here to support `thread-loader`
   ignoreIconIds?: string[];
 }
