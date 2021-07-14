672c3cc955a5a2c55ca222344e3ca36a121e85a5
th0r <grunin.ya@ya.ru>
2021-07-14 21:58:51 +0300

    Remove useless Angular test app

--- test/ng-app/log-loader.js
+++
@@ -1,9 +0,0 @@
-const {getOptions} = require('loader-utils');
-
-module.exports = function (content) {
-  const opts = getOptions(this);
-  if (!opts.filter || opts.filter.test(this.request)) {
-    console.log(`--- ${opts.id} ---\n\n`, content);
-  }
-  return content;
-};
